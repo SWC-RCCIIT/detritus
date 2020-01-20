c3182d6926e51bcd6a06c6fddf807f871093073b
Preetam Ghosh <diyelectronics99@gmail.com>
2020-01-20 17:39:19 +0530

    Day 1 Commit

---
+++ HWealth/app/src/main/java/com/example/h_wealth/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.h_wealth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt1,bt2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bt1 = findViewById(R.id.btn1);
+        bt2 = findViewById(R.id.btn2);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, loginActivity.class));
+            }
+        });
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, registrationActivity.class));
+
+            }
+        });
+
+
+    }
+}
